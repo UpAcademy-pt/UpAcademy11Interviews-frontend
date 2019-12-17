0822dcb45cfc9906d9e21b738ca7375976777ab3
João Silvestre <joaomsilvestre3@gmail.com>
2019-12-17 12:37:29 +0000

    commit users table

--- src/app/layout/shelf/shelf.component.ts
+++ src/app/layout/shelf/shelf.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { BsModalRef } from 'ngx-bootstrap';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-shelf',
@@ -6,7 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shelf.component.scss']
 })
 export class ShelfComponent implements OnInit {
-
+  public modalRef: BsModalRef;
+  public iconNew = faPlus;
+  rows = [
+    { name: 'Austin', gender: 'Male', company: 'Swimlane' },
+    { name: 'Dany', gender: 'Male', company: 'KFC' },
+    { name: 'Molly', gender: 'Female', company: 'Burger King' },
+  ];
+  columns = [
+    { prop: 'name' },
+    { name: 'Gender' },
+    { name: 'Company' }
+  ];
   constructor() { }
 
   ngOnInit() {
