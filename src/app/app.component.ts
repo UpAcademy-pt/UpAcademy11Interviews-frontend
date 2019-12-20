cd70f9e8300764412cf35e0a61d479549eed7b0a
Aluno Manhã <alunomanha@Altars-Mac-mini-3.local>
2019-12-20 09:41:47 +0000

    logout fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import { AccountApiService } from './core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -9,12 +11,20 @@ import { TranslateService } from '@ngx-translate/core';
 export class AppComponent {
   
   constructor(
-    translate: TranslateService
+    translate: TranslateService,
+
+    private router: Router,
+    private accountApi: AccountApiService
   ) {
     translate.addLangs(['en', 'pt']);
     translate.setDefaultLang('pt');
     const browserLang = translate.getBrowserLang();
     translate.use(browserLang.match(/en|pt/) ? browserLang : 'en');
+  }
+
+  public logout() {
+    this.accountApi.logout();
+    this.router.navigate(['/login']);
   }
 
 }
