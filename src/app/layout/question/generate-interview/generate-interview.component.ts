4998e0bc81c9ff5f1b4fe214a62712bf95ce9d48
João Silvestre <joaomsilvestre3@gmail.com>
2020-01-07 17:08:10 +0000

    commit generate interview

--- src/app/layout/question/generate-interview/generate-interview.component.ts
+++ src/app/layout/question/generate-interview/generate-interview.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { BsModalRef } from 'ngx-bootstrap';
+import { AttributeValue } from 'src/app/core/models/attribute-value';
+import { AttributeValueApiService } from 'src/app/core/services/attribute-value-service';
 
 @Component({
   selector: 'app-generate-interview',
@@ -8,16 +10,45 @@ import { BsModalRef } from 'ngx-bootstrap';
 })
 export class GenerateInterviewComponent implements OnInit {
 
+
+  Roles: AttributeValue[] = [];
+  Role = '';
+  activeImg = 0;
   constructor(
-    public bsModalRef: BsModalRef
+    public bsModalRef: BsModalRef,
+    public attributeValueApi: AttributeValueApiService
   ) { }
 
   ngOnInit() {
+
+    this.attributeValueApi.getByAttribute('Role').subscribe((data: AttributeValue[]) => {
+      this.Roles = data;
+    });
+
   }
+
+
 
   public createInterview() {
     this.bsModalRef.hide()
   }
 
- 
+  prevSlide() {
+    if (this.activeImg == 0) {
+      this.activeImg = 1; //length-1
+    }
+    else {
+      this.activeImg--;
+    }
+  }
+
+  nextSlide() {
+    if (this.activeImg == 1) {//length-1
+      this.activeImg = 0;
+    }
+    else {
+      this.activeImg++;
+    }
+  }
+
 }
