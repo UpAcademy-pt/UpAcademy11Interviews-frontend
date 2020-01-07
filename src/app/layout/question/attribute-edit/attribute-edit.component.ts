055e7540d9d27ccae8dcfcd58148120bb36dcf2e
Aluno Manhã <alunomanha@Altars-Mac-mini-3.local>
2020-01-07 13:29:36 +0000

    edit category now shows existing attribute types

--- src/app/layout/question/attribute-edit/attribute-edit.component.ts
+++ src/app/layout/question/attribute-edit/attribute-edit.component.ts
@@ -3,6 +3,8 @@ import { BsModalRef } from 'ngx-bootstrap';
 import { Attribute } from 'src/app/core/models/attribute';
 import { AttributeApiService } from 'src/app/core/services/attribute-service';
 import { DataService } from 'src/app/core';
+import { AttributeValue } from 'src/app/core/models/attribute-value';
+import { AttributeValueApiService } from 'src/app/core/services/attribute-value-service';
 
 
 @Component({
@@ -13,20 +15,27 @@ import { DataService } from 'src/app/core';
 export class AttributeEditComponent implements OnInit {
   public event: EventEmitter<any> = new EventEmitter();
   attribute: Attribute = new Attribute();
+  attributeValue : AttributeValue = new AttributeValue();
+  attributeValues : AttributeValue[] = [];
 
   id: number;
 
   constructor(
     public dataService: DataService,
     public attributeApi: AttributeApiService,
+    public attributeValueApi: AttributeValueApiService,
     public bsModalRef: BsModalRef) { 
-      this.attribute.category = "";
+      this.attributeValue.value = "";
     }
 
   ngOnInit() {
-    this.attributeApi.get(this.id).subscribe((data: Attribute) => {
-      console.log(data);
-      this.attribute.category = data.category;
+    this.attributeApi.get(this.id).subscribe((attribute: Attribute) => {
+      console.log(attribute);
+      this.attribute = attribute;
+      this.attributeValueApi.getByAttribute(this.attribute.category).subscribe((attributeValues: AttributeValue[]) => {
+        this.attributeValues = attributeValues;
+        console.log(attributeValues);
+      });
       });
   }
 
